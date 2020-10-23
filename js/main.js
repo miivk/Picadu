4cf6fa34f54743eb7616d7374ed053511a73f0cb
miivk <73361122+miivk@users.noreply.github.com>
2020-10-23 21:22:55 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// Cоздаём переменную,в которую положим кнопку меню
+let menuToggle = document.querySelector('#menu-toggle');
+// Создаём переменную,в которую положим меню
+let menu = document.querySelector('.sidebar')
+// Отслеживаем клик по кнопке меню и запускаем функцию
+menuToggle.addEventListener('click', function (event) {
+	//отменяем стандартное поведение ссылки
+	event.preventDefault();
+	//вешаем класс на меню,когда кликнули по кнопке меню
+	menu.classList.toggle('visible')
+})
